a8793b52c4c6915e81be12b0f411fe5969f58531
Grgur Grisogono <grguru@gmail.com>
2012-11-06 12:07:56 +0100

    initial page commit

--- README.md
+++
@@ -1,4 +0,0 @@
-Ext.data.proxy.WebSQL
-=====================
-
-WebSQL proxy for Sencha Touch 2. Use at your own risk. 
--- examples/list/app/store/Companies.js
+++
@@ -1,9 +0,0 @@
-Ext.define('App.store.Companies', {
-    extend : 'Ext.data.Store',
-
-    config : {
-        storeId  : 'companies',
-        autoLoad : true,
-        model    : 'App.model.Company'
-    }
-});
